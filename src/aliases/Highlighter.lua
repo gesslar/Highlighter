33240ecce7af15c19910ca3b73387ee4ad79dd11
gesslar <1266935+gesslar@users.noreply.github.com>
2025-06-23 20:00:46 -0400

    add-license-dependabot: 2025-06-23 19:59 - (#6)

--- src/aliases/Highlighter.lua
+++ src/aliases/Highlighter.lua
@@ -8,9 +8,11 @@ if command == "set" then
     -- Show current preference settings
     echo(f[[ Current {Highlighter.config.name} preferences: ]] .. "\n\n")
     echo(f[[   Fade: {Highlighter.prefs.fade} ]] .. "\n")
+    echo(f[[   Rollout: {Highlighter.prefs.rollout} ]] .. "\n")
     echo(f[[   Step: {Highlighter.prefs.step} ]] .. "\n")
     echo(f[[   Delay: {Highlighter.prefs.delay} ]] .. "\n")
     echo(f[[   Colour: {Highlighter.prefs.colour} ]] .. "\n")
+    echo(f[[   Alpha: {Highlighter.prefs.alpha} ]] .. "\n")
   end
 else
   -- Print out the highlighter instructions
